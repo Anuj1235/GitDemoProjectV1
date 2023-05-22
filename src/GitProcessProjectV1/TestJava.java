144b7ac2482adc1d02b6b27c2b5e1fde10552cae
91955 <91955@ANUJ>
2023-05-22 16:02:06 +0530

    First Commit

---
+++ src/GitProcessProjectV1/TestJava.java
@@ -0,0 +1,21 @@
+package GitProcessProjectV1;
+
+public class TestJava {
+
+public static void main(String[] args) {
+		
+		
+		String hungry = "yes";
+		
+		if (hungry=="yes") {
+			
+			System.out.println("eat something......");
+			
+		} else {
+			System.out.println("Do work......");
+		}
+		
+		
+		
+	}
+}
